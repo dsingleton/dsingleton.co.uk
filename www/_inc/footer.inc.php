ba56a4591b8863bf1b95c6bededc5c2efd699890
davids <davids@dsingleton.co.uk>
2009-07-22 15:02:47 +0100

    Add twitter/feed links to footer. Also add a footer

--- www/_inc/footer.inc.php
+++ www/_inc/footer.inc.php
@@ -4,6 +4,19 @@
 
  </div>
 
+ <div id="footer">
+     <ul id="selfPromotion">
+         <li class="twitter">
+             <a href="http://twitter.com/dsingleton">Follow me on twitter</a>
+         </li>
+         
+         <li class="feed">
+            <a href="/feed/blog.rss">Subscribe to my blog</a>
+         </li>
+     </ul>
+     
+ </div>
+
 <?php require_once dirname(__FILE__) . '/analytics.html' ?>
  
 </body>
