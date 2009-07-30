0510ae34f20eeba5190703c692c4f3dc0c42df6a
davids <davids@dsingleton.co.uk>
2009-07-30 23:21:21 +0100

    domain based conf

--- init.php
+++ init.php
@@ -1,5 +1,7 @@
 <?php
 
-require_once dirname(__FILE__) . '/etc/dev.php';
+$env = ($_SERVER["SERVER_NAME"] == 'dsingleton.co.uk') ? 'prod' : 'dev';
+
+require_once dirname(__FILE__) . '/etc/' . $env . '.php';
 
 require_once dirname(__FILE__) . '/lib/lib.php';
