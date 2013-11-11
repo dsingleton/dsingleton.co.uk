085020188ee28a9c15e755592bd4f77e0fbd35f5
David Singleton <david@dsingleton.co.uk>
2013-11-11 23:39:27 +0000

    Very simple build task, copy to dist/. No different from app/ for now, but this is the workflow I want when app/ becomes more dynamic

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,22 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-		
+		copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: 'app',
+          dest: 'dist',
+          src: ['**/*']
+        }]
+      }
+    }
   });
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerTask('build', [
+    'copy:dist',
+  ]);
+
 };
