d53aac69090f8c1e0d53b7b51140fe5aabf1c0cf
David Singleton <david@dsingleton.co.uk>
2013-11-15 21:58:30 +0000

    Add a server task

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,13 +10,32 @@ module.exports = function(grunt) {
           src: ['**/*']
         }]
       }
+    },
+
+    connect: {
+      options: {
+        hostname: 'localhost',
+        port: 9000
+      },
+      dist: {
+        options: {
+          open: true,
+          keepalive: true,
+          base: 'dist'
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 
   grunt.registerTask('build', [
     'copy:dist',
   ]);
 
+  grunt.registerTask('server', [
+    'build',
+    'connect:dist'
+  ]);
 };
